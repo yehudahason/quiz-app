16f09a1362073275d0c499f56de36664284d0839
Yehuda Hason <yehudaxt@gmail.com>
2026-01-25 16:08:32 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Page2 from './pages/Page2'
