d1ace425aef5a63c68732f4c2ad2a745a9c4883f
Yehuda Hason <yehudaxt@gmail.com>
2026-01-30 11:15:15 +0200

    layout

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -6,12 +6,14 @@ const Section1 = ({ selected }: Props) => {
   return (
     <>
       {selected ? (
-        <div className="start-con1 quiz">
-          <h1>
-            <span className="a">Welcome to the</span> <br />
-            <span className="b"> Frontend Quiz!</span>
-          </h1>
-          <p>Pick a subject to get started.</p>
+        <div className="quiz">
+          <div className="upper">
+            <span className="stage">Question 6 of 10</span>
+            <h1>What does HTML stand for?</h1>
+          </div>
+          <div className="bar">
+            <div className="progress"></div>
+          </div>
         </div>
       ) : (
         <>
